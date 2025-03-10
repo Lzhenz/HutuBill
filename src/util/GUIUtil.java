3b9c0523866e7749fcae9ddb8d11ea79a04134ac
Lzhenz <2521071907@qq.com>
2025-03-11 00:34:40 +0800

    测试Mac系统下的路径

--- src/util/GUIUtil.java
+++ src/util/GUIUtil.java
@@ -1,5 +1,25 @@
 package util;
 
+import javax.swing.*;
+
 public class GUIUtil {
+
     private static String imageFolder = "";
+
+    public static void setImageFolder(JButton b , String fileName , String tip) {
+        // 识别当前电脑环境
+        String osName = System.getProperty("os.name");
+        if (osName.startsWith("Windows")){
+            // Win
+            imageFolder = "C:/Project/imageFolder";
+        } else if (osName.startsWith("Mac os")) {
+            // Mac os
+            imageFolder = "/Users/zhenz/study/imageFolder";
+        }
+    }
+
+    public static void main(String[] args) {
+        GUIUtil.setImageFolder(new JButton() , "hello" , "hello");
+    }
+
 }
