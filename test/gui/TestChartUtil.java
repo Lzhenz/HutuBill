efc12d2ab659c54921fa097a3b96880dbdd50bc4
Lzhenz <2521071907@qq.com>
2025-05-05 15:53:07 +0900

    更新条状图进度

---
+++ test/gui/TestChartUtil.java
@@ -0,0 +1,18 @@
+package gui;
+
+import util.ChartUtil;
+
+import javax.swing.*;
+
+public class TestChartUtil {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        JPanel jPanel = new JPanel();
+        JLabel jLabel = new JLabel();
+
+        ChartUtil chartUtil = new ChartUtil();
+
+
+
+    }
+}
